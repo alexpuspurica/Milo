91dc55b02059fb4e646cea169394a4b53bfc1afa
michal-47 <michal.hrotko@gmail.com>
2026-04-22 16:03:21 +0200

    API update

---
+++ Whoop/Write_to_Whoop_ENV.py
@@ -0,0 +1,7 @@
+import getpass
+usernameintowhoop = input("Enter your WHOOP E-mail: ")
+passwordintowhoop = getpass.getpass("Enter your WHOOP Password: ")
+
+with open (".env", "w") as f:
+    f.write(f'USERNAMEinENV={usernameintowhoop}"\n')
+    f.write(f'PASSWORDinENV={passwordintowhoop}"\n')
